3d6010d2fe66c8471430c60a464c99c92185e368
Seongsu Jeong <holywaterj0410@gmail.com>
2023-05-31 22:20:15 +0900

    feature: add date at posts & post

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { allPosts } from '@/.contentlayer/generated';
 import Link from 'next/link';
+import dayjs from 'dayjs';
 
 export default function Home() {
   const posts = allPosts.sort((a, b) => {
@@ -11,7 +12,8 @@ export default function Home() {
       {posts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
-            <h2>{post.title}</h2>
+            <h2 style={{ marginBottom: '0.25em' }}>{post.title}</h2>
+            <span>{dayjs(post.date).format('YYYY-MM-DD')}</span>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
