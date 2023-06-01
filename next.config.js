c79a98ac73836dad543046fa152bc913fcbb2d6b
Seongsu Jeong <holywaterj0410@gmail.com>
2023-06-01 17:42:57 +0900

    fix: config - swc 사용하도록

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = withContentlayer(nextConfig);
