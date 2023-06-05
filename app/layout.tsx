b408af34c7fe978a10a24b628f5825e3aebcba3f
Seongsu Jeong <holywaterj0410@gmail.com>
2023-06-05 10:09:19 +0900

    add: gtag & prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,43 @@
-import Link from "next/link"
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Analytics } from "@/components/analytics"
-import { ModeToggle } from "@/components/mode-toggle"
+import Link from 'next/link';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] })
+import './globals.css';
+
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@/components/analytics';
+import { ModeToggle } from '@/components/mode-toggle';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Dev. Holywater Jeong",
-  description: "사람에게 유익하고 선한 서비스가 가득한 온라인 세상을 만들어가는 개발자 / Frontend Developer",
-}
+  title: 'Dev. Holywater Jeong',
+  description:
+    '사람에게 유익하고 선한 서비스가 가득한 온라인 세상을 만들어가는 개발자 / Frontend Developer',
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9MVWVCL458"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-9MVWVCL458');
+          `}
+        </Script>
+      </head>
       <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
@@ -39,5 +58,5 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
